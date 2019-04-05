411b1a5083e8de86b65e3465d420d5720f867a11
Chris Opperwall <chriso@ifixit.com>
2019-04-04 21:29:42 -0700

    Use prettier for formatting

--- src/Form.js
+++ src/Form.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 
 export default function Form({ numDigits, onSubmit }) {
   const [digits, setDigits] = useState(Array(numDigits).fill(""));
@@ -14,15 +14,14 @@ export default function Form({ numDigits, onSubmit }) {
       return;
     }
 
-    onSubmit(Number(digits.join('')));
+    onSubmit(Number(digits.join("")));
   }
 
   function handleChangeByIndex(index) {
     return function onChangeHandler(event) {
       setError(false);
-      const number = event.target.value === "" ?
-                      "" :
-                      Number(event.target.value);
+      const number =
+        event.target.value === "" ? "" : Number(event.target.value);
 
       if (Number.isNaN(number)) return;
 
@@ -37,7 +36,7 @@ export default function Form({ numDigits, onSubmit }) {
       } else if (Number.isInteger(number) && focus < digits.length - 1) {
         setFocus(focus => focus + 1);
       }
-    }
+    };
   }
 
   useEffect(() => {
@@ -47,40 +46,48 @@ export default function Form({ numDigits, onSubmit }) {
   }, [digits, focus]);
 
   return (
-    <form style={{
-            display: 'flex',
-            flexDirection: 'column',
-            minWidth: '300px'
-          }}
-          onSubmit={handleSubmit}  >
-      <div style={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          marginBottom: '10px'
-        }}>
+    <form
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        minWidth: "300px"
+      }}
+      onSubmit={handleSubmit}
+    >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          marginBottom: "10px"
+        }}
+      >
         {digits.map((digit, index) => (
-          <input type="text"
-                style={{
-                  maxWidth: '3rem',
-                  lineHeight: '5vh',
-                  fontSize: '2rem',
-                  textAlign: 'center',
-                  padding: '5px',
-                  borderRadius: '5px',
-                  borderWidth: '1px',
-                  borderColor: error ? 'red' : 'inherit'
-                }}
-                onChange={handleChangeByIndex(index)}
-                key={index}
-                minLength="0"
-                maxLength="1"
-                ref={node => {
-                  if (node && index === focus) node.focus()
-                }}
-                value={digit} />
+          <input
+            type="text"
+            style={{
+              maxWidth: "3rem",
+              lineHeight: "5vh",
+              fontSize: "2rem",
+              textAlign: "center",
+              padding: "5px",
+              borderRadius: "5px",
+              borderWidth: "1px",
+              borderColor: error ? "red" : "inherit"
+            }}
+            onChange={handleChangeByIndex(index)}
+            key={index}
+            minLength="0"
+            maxLength="1"
+            ref={node => {
+              if (node && index === focus) node.focus();
+            }}
+            value={digit}
+          />
         ))}
       </div>
-      <button style={{height: '30px'}} type="submit">Submit</button>
+      <button style={{ height: "30px" }} type="submit">
+        Submit
+      </button>
     </form>
   );
 }
@@ -88,4 +95,4 @@ export default function Form({ numDigits, onSubmit }) {
 Form.propTypes = {
   numDigits: PropTypes.number,
   onSubmit: PropTypes.func
-}
+};
